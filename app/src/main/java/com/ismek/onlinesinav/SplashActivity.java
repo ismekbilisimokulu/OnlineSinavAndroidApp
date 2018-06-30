880bb00d34a2b3e8a3ed66e1684bde6a617a2cc1
mekhti11 <mekhti11@gmail.com>
2018-06-30 11:59:47 +0300

    splash ekranı yapıldı , SharedPreferencesUtil eklendi

--- app/src/main/java/com/ismek/onlinesinav/SplashActivity.java
+++ app/src/main/java/com/ismek/onlinesinav/SplashActivity.java
@@ -1,13 +1,36 @@
 package com.ismek.onlinesinav;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASHTIME = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(SPLASHTIME);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
