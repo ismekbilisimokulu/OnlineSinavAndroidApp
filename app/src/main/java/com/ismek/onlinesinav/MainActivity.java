a5eb2a14adf8858c8491ea45c4442104342bb2b4
Lab01-30 <bilisimteknolojileriokulu@gmail.com>
2018-07-08 13:24:11 +0300

    Sinav sayfasi (webview) temeli oluşturuldu.

--- app/src/main/java/com/ismek/onlinesinav/MainActivity.java
+++ app/src/main/java/com/ismek/onlinesinav/MainActivity.java
@@ -1,22 +1,23 @@
 package com.ismek.onlinesinav;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ImageView gifImg;
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        gifImg = findViewById(R.id.gifImg);
-
-        Glide.with(getApplicationContext()).asGif().load(R.drawable.loading).into(gifImg);
+    protected void onViewReady(Bundle savedInstanceState, Intent intent) {
+        super.onViewReady(savedInstanceState, intent);
     }
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+
+
 }
--- app/src/main/java/com/ismek/onlinesinav/SinavActivity.java
+++ app/src/main/java/com/ismek/onlinesinav/SinavActivity.java
@@ -1,10 +1,23 @@
 package com.ismek.onlinesinav;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import butterknife.BindView;
 
 public class SinavActivity extends BaseActivity {
 
+    @BindView(R.id.webView)
+    public WebView webView;
+
+    @Override
+    protected void onViewReady(Bundle savedInstanceState, Intent intent) {
+        super.onViewReady(savedInstanceState, intent);
+
+        //webView.getSettings().setBuiltInZoomControls(true);
+    }
 
     @Override
     protected int getContentView() {
