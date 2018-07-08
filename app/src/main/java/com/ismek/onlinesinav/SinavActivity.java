8d6c7a292a7b03695e6d43382cec812e52ce4cd6
Lab01-30 <bilisimteknolojileriokulu@gmail.com>
2018-07-08 12:04:03 +0300

    header ve katıl imageleri yüklendi.

---
+++ app/src/main/java/com/ismek/onlinesinav/SinavActivity.java
@@ -0,0 +1,13 @@
+package com.ismek.onlinesinav;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SinavActivity extends BaseActivity {
+
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_sinav;
+    }
+}
